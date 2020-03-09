321e2f1db623203cb483cec9f4d86b21a2343c7d
Horia Coman <horia141@gmail.com>
2020-03-10 00:22:25 +0200

    Merged branch feature/release-scripts-v4 into develop

--- scripts/prepare-release.sh
+++ scripts/prepare-release.sh
@@ -4,6 +4,7 @@ set -e
 
 RELEASE_VERSION=$1
 RELEASE_NOTES_PATH="docs/releases/version-${RELEASE_VERSION}.md"
+RELEASE_DATE=$(date +"%Y/%m/%d")
 
 if [[ $(git rev-parse --abbrev-ref HEAD) != develop ]]
 then
@@ -13,8 +14,9 @@ fi
 
 git pull
 git checkout -b "release/v${RELEASE_VERSION}"
-cat Config | sed -E "s/VERSION=.+/VERSION=${RELEASE_VERSION}/g" > Config
+cat Config | sed -E "s/VERSION=.+/VERSION=${RELEASE_VERSION}/g" > Config.bak
+mv Config.bak Config
 cp docs/releases/template.md ${RELEASE_NOTES_PATH}
-sed -i "s/{{release_version}}/${RELEASE_VERSION}/g" ${RELEASE_NOTES_PATH}
-sed -i "s/{{release_date}}/$(date +\"%Y/%m/%d\")/g" ${RELEASE_NOTES_PATH}
+sed -i -E "s/{{release_version}}/${RELEASE_VERSION}/g" ${RELEASE_NOTES_PATH}
+sed -i -E "s|{{release_date}}|${RELEASE_DATE}|g" ${RELEASE_NOTES_PATH}
 git add ${RELEASE_NOTES_PATH}
