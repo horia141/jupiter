79eaec536343778d58028ac68e166261e34dde15
Horia Coman <horia141@gmail.com>
2020-03-13 23:20:48 +0200

    Make the vacations adding command modify the table too

--- src/commands/vacations_add.py
+++ src/commands/vacations_add.py
@@ -2,8 +2,11 @@ import datetime
 import logging
 
 import pendulum
+from notion.client import NotionClient
 
 import commands.command as command
+import lockfile
+import space_utils
 import storage
 
 LOGGER = logging.getLogger(__name__)
@@ -35,10 +38,15 @@ class VacationsAdd(command.Command):
 
         # Load local storage
 
+        the_lock = lockfile.load_lock_file()
         workspace = storage.load_workspace()
         LOGGER.info("Loaded workspace data")
 
-        # Apply changes
+        # Prepare Notion connection
+
+        client = NotionClient(token_v2=workspace["token"])
+
+        # Apply changes locally
 
         new_vacation = {
             "start_date": datetime.date(start_date.year, start_date.month, start_date.day),
@@ -46,7 +54,14 @@ class VacationsAdd(command.Command):
         }
         workspace["vacations"].append(new_vacation)
         workspace["vacations"] = sorted(workspace["vacations"], key=lambda vac: vac["start_date"])
-
-        # Save changes
         storage.save_workspace(workspace)
-        LOGGER.info("Saved workspace data")
+        LOGGER.info("Applied local changes")
+
+        # Apply changes in Notion
+
+        vacations_page = space_utils.find_page_from_space_by_id(client, the_lock["vacations"]["root_page_id"])
+        new_vacation_row = vacations_page.collection.add_row()
+        new_vacation_row.title = "New vacation"
+        new_vacation_row.start_date = new_vacation["start_date"]
+        new_vacation_row.end_date = new_vacation["end_date"]
+        LOGGER.info("Applied Notion changes")
