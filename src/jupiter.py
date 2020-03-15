8fd7dd693ac67301b73f585452354369e2df661e
Horia Coman <horia141@gmail.com>
2020-03-15 17:00:43 +0200

    Add command to change vacation name

--- src/jupiter.py
+++ src/jupiter.py
@@ -8,6 +8,7 @@ import commands.upsert_big_plans as upsert_big_plans
 import commands.upsert_tasks as upsert_tasks
 import commands.vacations_add as vacations_add
 import commands.vacations_remove as vacations_remove
+import commands.vacations_set_name as vacations_set_name
 import commands.vacations_show as vacations_show
 import commands.vacations_sync as vacations_sync
 import commands.workspace_init as workspace_init
@@ -28,6 +29,7 @@ def main():
         workspace_sync.WorkspaceSync(),
         vacations_add.VacationsAdd(),
         vacations_remove.VacationsRemove(),
+        vacations_set_name.VacationsSetName(),
         vacations_show.VacationsShow(),
         vacations_sync.VacationsSync(),
         create_project.CreateProject(),
