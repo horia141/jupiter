786ad63cb3a05e7ea635bccab1b8256b2cba086a
Horia Coman <horia141@gmail.com>
2020-03-16 17:21:34 +0200

    Add linter for dockerfiles

---
+++ scripts/lint-dockerfile.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+set -e
+
+hadolint Dockerfile
--- scripts/lint.sh
+++ scripts/lint.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+./scripts/lint-dockerfile.sh
 ./scripts/lint-workflows.sh
 ./scripts/lint-docs.sh
 ./scripts/lint-scripts.sh
