5105dfec0babd90812d3bf777546517b2a5d17cf
Horia Coman <horia141@gmail.com>
2020-03-21 23:38:58 +0200

    Install Python dependencies globally when running in CI

--- scripts/setup-for-ci.sh
+++ scripts/setup-for-ci.sh
@@ -2,9 +2,15 @@
 
 set -echo
 
+# Prepare dependencies
+
 apt-get install python3-setuptools
 curl -sSL https://get.rvm.io | bash -s stable
 rvm install ruby-2.4.2
 pip3 install pylint yamllint pyflakes bandit
 gem install mdl
 docker pull hadolint/hadolint:latest-debian
+
+# Prepare environment
+
+pip3 install -r requirements.txt
