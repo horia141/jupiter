0c962e1e09212b67eb88adc008f24d0cdab9002a
Horia Coman <horia141@gmail.com>
2020-04-23 20:24:22 +0300

    Also do the sudo inside the script

--- scripts/setup-for-ci.sh
+++ scripts/setup-for-ci.sh
@@ -4,7 +4,7 @@ set -e
 
 # Prepare environment
 
-apt-get install python3-setuptools gnupg2
+sudo apt-get install python3-setuptools gnupg2
 python -m pip install --upgrade pip
 pip install -r requirements-dev.txt
 gem install mdl
