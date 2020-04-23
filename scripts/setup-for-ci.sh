3d72e9e59ee67cd5a44a144556880b5c08f06066
Horia Coman <horia141@gmail.com>
2020-04-23 18:47:45 +0300

    This one might work

--- scripts/setup-for-ci.sh
+++ scripts/setup-for-ci.sh
@@ -2,15 +2,16 @@
 
 set -e
 
-# Prepare dependencies
+# Prepare environment
 
 apt-get install python3-setuptools
 curl -sSL https://get.rvm.io | bash -s stable
 rvm install ruby-2.4.2
-pip3 install -r requirements-dev.txt
+python -m pip install --upgrade pip
+pip install -r requirements-dev.txt
 gem install mdl
 docker pull hadolint/hadolint:latest-debian
 
-# Prepare environment
+# Prepare dependencies
 
-pip3 install -r requirements.txt
+pip install -r requirements.txt
