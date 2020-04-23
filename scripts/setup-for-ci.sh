d330c395ab8251b5f133c406c2ddc30a4dd572a3
Horia Coman <horia141@gmail.com>
2020-04-23 19:51:28 +0300

    Other ruby

--- scripts/setup-for-ci.sh
+++ scripts/setup-for-ci.sh
@@ -5,9 +5,6 @@ set -e
 # Prepare environment
 
 apt-get install python3-setuptools gnupg2
-gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
-curl -sSL https://get.rvm.io | bash -s stable
-rvm install ruby-2.4.2
 python -m pip install --upgrade pip
 pip install -r requirements-dev.txt
 gem install mdl
