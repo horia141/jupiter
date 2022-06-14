5b19ea8bf493d2f7aa3d96458e85c1c8c7e971e4
Horia Coman <horia141@gmail.com>
2022-06-14 16:16:00 +0300

    Merged branch feature/better-setup-for-dev into develop

--- scripts/setup-for-dev.sh
+++ scripts/setup-for-dev.sh
@@ -5,7 +5,8 @@ set -e
 # Prepare environment
 
 brew install shellcheck
-curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
-poetry install --no-interaction --no-ansi
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 -
+# shellcheck disable=SC1090 # poetry is special here
+~/.poetry/bin/poetry install --no-interaction --no-ansi
 sudo gem install mdl
 docker pull hadolint/hadolint:latest-debian
