67900439552f855d26f4ddc7d7d78ccc396ba0cd
Horia Coman <horia141@gmail.com>
2023-06-29 21:32:52 -0700

    Change default project for a workspace

--- src/webui/app/routes/workspace/settings.tsx
+++ src/webui/app/routes/workspace/settings.tsx
@@ -7,14 +7,16 @@ import {
   CardHeader,
   FormControl,
   InputLabel,
+  MenuItem,
   OutlinedInput,
+  Select,
   Stack,
 } from "@mui/material";
 import type { ActionArgs, LoaderArgs } from "@remix-run/node";
 import { json, redirect } from "@remix-run/node";
 import { useActionData, useTransition } from "@remix-run/react";
 import { StatusCodes } from "http-status-codes";
-import { ApiError } from "jupiter-gen";
+import { ApiError, Project } from "jupiter-gen";
 import { z } from "zod";
 import { parseForm } from "zodix";
 import { getLoggedInApiClient } from "~/api-clients";
@@ -31,6 +33,7 @@ import { getSession } from "~/sessions";
 const WorkspaceSettingsFormSchema = {
   intent: z.string(),
   name: z.string(),
+  defaultProject: z.string(),
 };
 
 export const handle = {
@@ -39,12 +42,29 @@ export const handle = {
 
 export async function loader({ request }: LoaderArgs) {
   const session = await getSession(request.headers.get("Cookie"));
+  const summaryResponse = await getLoggedInApiClient(
+    session
+  ).getSummaries.getSummaries({
+    allow_archived: false,
+    include_default_project: true,
+    include_vacations: false,
+    include_projects: true,
+    include_inbox_tasks: false,
+    include_habits: false,
+    include_chores: false,
+    include_big_plans: false,
+    include_smart_lists: false,
+    include_metrics: false,
+    include_persons: false,
+  });
   const result = await getLoggedInApiClient(session).workspace.loadWorkspace(
     {}
   );
 
   return json({
     workspace: result.workspace,
+    defaultProject: summaryResponse.default_project as Project,
+    allProjects: summaryResponse.projects as Array<Project>,
   });
 }
 
@@ -62,6 +82,16 @@ export async function action({ request }: ActionArgs) {
             should_change: true,
             value: { the_name: form.name },
           },
+        });
+
+        return redirect(`/workspace/settings`);
+      }
+
+      case "change-default-project": {
+        await getLoggedInApiClient(
+          session
+        ).workspace.changeWorkspaceDefaultProject({
+          default_project_ref_id: { the_id: form.defaultProject },
         });
 
         return redirect(`/workspace/settings`);
@@ -121,6 +151,48 @@ export default function Settings() {
                   value="update"
                 >
                   Save
+                </Button>
+              </ButtonGroup>
+            </CardActions>
+          </Card>
+
+          <Card>
+            <CardHeader title="Default Project" />
+            <CardContent>
+              <Stack spacing={2} useFlexGap>
+                <FormControl fullWidth>
+                  <InputLabel id="defaultProject">Default Project</InputLabel>
+                  <Select
+                    labelId="defaultProject"
+                    name="defaultProject"
+                    readOnly={!inputsEnabled}
+                    defaultValue={loaderData.defaultProject.ref_id.the_id}
+                    label="Default Project"
+                  >
+                    {loaderData.allProjects.map((p) => (
+                      <MenuItem key={p.ref_id.the_id} value={p.ref_id.the_id}>
+                        {p.name.the_name}
+                      </MenuItem>
+                    ))}
+                  </Select>
+                  <FieldError
+                    actionResult={actionData}
+                    fieldName="/deafult_project_ref_id"
+                  />
+                </FormControl>
+              </Stack>
+            </CardContent>
+
+            <CardActions>
+              <ButtonGroup>
+                <Button
+                  variant="contained"
+                  disabled={!inputsEnabled}
+                  type="submit"
+                  name="intent"
+                  value="change-default-project"
+                >
+                  Change Default Project
                 </Button>
               </ButtonGroup>
             </CardActions>
