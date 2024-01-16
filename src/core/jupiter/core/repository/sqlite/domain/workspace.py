e8bb34fcced991b565ea3c67fd91be0dc6228cd3
Horia Coman <horia141@gmail.com>
2024-01-16 23:37:19 +0200

    Workspaces be there

--- src/core/jupiter/core/repository/sqlite/domain/workspace.py
+++ src/core/jupiter/core/repository/sqlite/domain/workspace.py
@@ -41,53 +41,5 @@ class SqliteWorkspaceRepository(
             realm_codec_registry,
             connection,
             metadata,
-            Table(
-                "workspace",
-                metadata,
-                Column("ref_id", Integer, primary_key=True, autoincrement=True),
-                Column("version", Integer, nullable=False),
-                Column("archived", Boolean, nullable=False),
-                Column("created_time", DateTime, nullable=False),
-                Column("last_modified_time", DateTime, nullable=False),
-                Column("archived_time", DateTime, nullable=True),
-                Column("name", String(100), nullable=False),
-                Column("default_project_ref_id", Integer, nullable=True),
-                Column("feature_flags", JSON, nullable=False),
-                keep_existing=True,
-            ),
             not_found_err_cls=WorkspaceNotFoundError,
-        )
-
-    def _entity_to_row(self, entity: Workspace) -> RowType:
-        return {
-            "version": entity.version,
-            "archived": entity.archived,
-            "created_time": entity.created_time.to_db(),
-            "last_modified_time": entity.last_modified_time.to_db(),
-            "archived_time": entity.archived_time.to_db()
-            if entity.archived_time
-            else None,
-            "name": str(entity.name),
-            "default_project_ref_id": entity.default_project_ref_id.as_int(),
-            "feature_flags": {f.value: v for f, v in entity.feature_flags.items()},
-        }
-
-    def _row_to_entity(self, row: RowType) -> Workspace:
-        return Workspace(
-            ref_id=EntityId.from_raw(str(row["ref_id"])),
-            version=row["version"],
-            archived=row["archived"],
-            created_time=Timestamp.from_db(row["created_time"]),
-            archived_time=Timestamp.from_db(row["archived_time"])
-            if row["archived_time"]
-            else None,
-            last_modified_time=Timestamp.from_db(row["last_modified_time"]),
-            events=[],
-            name=WorkspaceName.from_raw(row["name"]),
-            default_project_ref_id=EntityId.from_raw(
-                str(row["default_project_ref_id"]),
-            ),
-            feature_flags={
-                WorkspaceFeature(f): v for f, v in row["feature_flags"].items()
-            },
         )
