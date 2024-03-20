0778d3a36c9d375ae28a70dc362242bdddf0f255
Horia Coman <horia141@gmail.com>
2024-03-21 00:46:56 +0200

    Merged branch bugfix/bad-migration into develop

---
+++ src/core/migrations/versions/2024_03_21_00_44_backup_feature_working_mem.py
@@ -0,0 +1,29 @@
+"""Backup feature working mem
+
+Revision ID: 626b770f8db6
+Revises: e57d34a43026
+Create Date: 2024-03-21 00:44:23.130986
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '626b770f8db6'
+down_revision = 'e57d34a43026'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.execute(
+        """
+        UPDATE workspace
+        SET feature_flags = json_set(feature_flags, '$.working-mem', 'true')
+        """
+    )
+
+
+def downgrade() -> None:
+    pass
