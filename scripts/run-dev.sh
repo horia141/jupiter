a1a2b029ede47b855be2eb387aee05608d46d2f6
Horia Coman <horia141@gmail.com>
2024-04-20 11:05:13 +0300

    A nice little process runner

--- scripts/run-dev.sh
+++ scripts/run-dev.sh
@@ -2,5 +2,8 @@
 
 set -ex
 
-(cd src/webui && PORT=10020 npm run dev) &
-(cd src/webapi && SQLITE_DB_URL=sqlite+aiosqlite:///../../.build-cache/juiter-fixed.sqlite  python -m watchfiles jupiter.webapi.jupiter.sync_main . ../core)
+npx mprocs --config scripts/mprocs.dev.yaml
+
+# npx mprocs \
+#   "(cd src/webui && PORT=10020 npm run dev)" \
+#  "(cd src/webapi && SQLITE_DB_URL=sqlite+aiosqlite:///../../.build-cache/juiter-fixed.sqlite  python -m watchfiles jupiter.webapi.jupiter.sync_main . ../core)"
