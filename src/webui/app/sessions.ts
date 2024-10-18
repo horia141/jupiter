6378e31106bb3faa758b2b01348a88eba5e15137
Horia Coman <horia141@gmail.com>
2024-10-18 13:40:01 +0200

    Merged branch bugfix/a-very-tricky-bugfix into develop

--- src/webui/app/sessions.ts
+++ src/webui/app/sessions.ts
@@ -25,7 +25,7 @@ const { getSession, commitSession, destroySession } =
       httpOnly: true,
       maxAge: 60 * 60 * 24 * 30, // 30 days
       path: "/",
-      sameSite: "strict",
+      sameSite: "lax", // Not strict because of https://github.com/oauth2-proxy/oauth2-proxy/issues/830
       secure: GLOBAL_PROPERTIES.env === Env.LOCAL ? false : true,
       secrets: [GLOBAL_PROPERTIES.sessionCookieSecret],
     },
