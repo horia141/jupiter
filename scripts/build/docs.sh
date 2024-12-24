4bac816919cefde2d4c5729899796d41327e649c
Horia Coman <horia141@gmail.com>
2024-12-24 17:34:58 +0200

    Merged branch bugfix/work-on-it into develop

--- scripts/build/docs.sh
+++ scripts/build/docs.sh
@@ -4,12 +4,12 @@ set -ex
 
 mkdir -p .build-cache/docs
 
-(cd src/docs && poetry install --only main --no-interaction --no-ansi --no-root)
-
 source src/Config.global
 
 export PUBLIC_NAME
 export AUTHOR
 export COPYRIGHT
+
+(cd src/docs && poetry install --only main --no-interaction --no-ansi --no-root)
 
 mkdocs build --config-file src/docs/mkdocs.yml --site-dir ../../.build-cache/docs --clean
