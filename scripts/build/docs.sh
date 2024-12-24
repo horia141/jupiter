df2ed7bed380f3cfb55b21cd5dd1d7961532e0be
Horia Coman <horia141@gmail.com>
2024-12-24 17:13:15 +0200

    Merged branch feature/better-docs-integration into develop

--- scripts/build/docs.sh
+++ scripts/build/docs.sh
@@ -6,4 +6,10 @@ mkdir -p .build-cache/docs
 
 (cd src/docs && poetry install --only main --no-interaction --no-ansi --no-root)
 
+source src/Config.global
+
+export PUBLIC_NAME
+export AUTHOR
+export COPYRIGHT
+
 mkdocs build --config-file src/docs/mkdocs.yml --site-dir ../../.build-cache/docs --clean
