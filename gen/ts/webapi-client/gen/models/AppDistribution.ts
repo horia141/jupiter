d771a3ce3d7b7e9685e216a729d9891276e9033c
Horia Coman <horia141@gmail.com>
2025-01-04 10:41:02 +0200

    Merged branch bugfix/add-stuff into develop

---
+++ gen/ts/webapi-client/gen/models/AppDistribution.ts
@@ -0,0 +1,14 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+/**
+ * The distribution channel of the app.
+ */
+export enum AppDistribution {
+    WEB = 'web',
+    MAC_WEB = 'mac-web',
+    MAC_STORE = 'mac-store',
+    APP_STORE = 'app-store',
+    GOOGLE_PLAY_STORE = 'google-play-store',
+}
