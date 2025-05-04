8ab8365670de10f9a1033e73242cdd7ce05dc6fa
Horia Coman <horia141@gmail.com>
2025-05-04 17:33:37 +0300

    Working it

--- src/core/jupiter/core/domain/application/home/home_config.py
+++ src/core/jupiter/core/domain/application/home/home_config.py
@@ -1,8 +1,13 @@
 """The home config domain application."""
 
+from jupiter.core.domain.application.home.home_widget import HomeWidget
+from jupiter.core.domain.core.entity_icon import EntityIcon
 from jupiter.core.framework.base.entity_id import EntityId
+from jupiter.core.framework.base.entity_name import EntityName
 from jupiter.core.framework.context import DomainContext
 from jupiter.core.framework.entity import (
+    ContainsMany,
+    IsRefId,
     ParentLink,
     TrunkEntity,
     create_entity_action,
@@ -11,9 +16,43 @@ from jupiter.core.framework.entity import (
 )
 from jupiter.core.framework.errors import InputValidationError
 from jupiter.core.framework.update_action import UpdateAction
+from jupiter.core.framework.value import CompositeValue, value
 
 MAX_KEY_HABITS = 3
 MAX_KEY_METRICS = 3
+
+@value
+class HomeDesktopTabConfig(CompositeValue):
+    """A tab on the home page."""
+
+    name: EntityName
+    icon: EntityIcon | None
+    widgets: list[list[EntityId]]
+
+
+@value
+class HomeDesktopConfig(CompositeValue):
+    """A desktop config for the home page."""
+
+    home_config: ParentLink
+    tabs: list[HomeDesktopTabConfig]
+
+
+@value
+class HomeMobileTabConfig(CompositeValue):
+    """A tab on the home page."""
+
+    name: EntityName
+    icon: EntityIcon | None
+    widgets: list[list[EntityId]]
+
+
+@value
+class HomeMobileConfig(CompositeValue):
+    """A mobile config for the home page."""
+
+    home_config: ParentLink
+    tabs: list[HomeMobileTabConfig]
 
 
 @entity
@@ -24,6 +63,11 @@ class HomeConfig(TrunkEntity):
 
     key_habits: list[EntityId]
     key_metrics: list[EntityId]
+
+    desktop_config: HomeDesktopConfig
+    mobile_config: HomeMobileConfig
+
+    widgets = ContainsMany(HomeWidget, home_config_ref_id=IsRefId())
 
     @staticmethod
     @create_entity_action
