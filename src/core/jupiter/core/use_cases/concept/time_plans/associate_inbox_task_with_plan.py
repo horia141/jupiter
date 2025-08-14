746bfe87f6763c416da238d5c7bb266251bd52f7
Horia Coman <horia141@gmail.com>
2025-08-15 00:07:32 +0300

    Merged branch feature/associate-bp-for-it into develop

--- src/core/jupiter/core/use_cases/concept/time_plans/associate_inbox_task_with_plan.py
+++ src/core/jupiter/core/use_cases/concept/time_plans/associate_inbox_task_with_plan.py
@@ -124,8 +124,8 @@ class TimePlanAssociateInboxTaskWithPlanUseCase(
                         context.domain_context,
                         time_plan_ref_id=time_plan.ref_id,
                         big_plan_ref_id=big_plan.ref_id,
-                        kind=args.kind,
-                        feasability=args.feasability,
+                        kind=TimePlanActivityKind.MAKE_PROGRESS,
+                        feasability=TimePlanActivityFeasability.NICE_TO_HAVE,
                     )
                     new_time_plan_activity = await generic_creator(
                         uow, progress_reporter, new_time_plan_activity
